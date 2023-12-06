2a828199620c1bdf811a46e49880c0c58c1dc3ea
GeralBahamondes <131837252+GeralBahamondes@users.noreply.github.com>
2023-12-05 21:26:54 -0300

    Add files via upload

---
+++ NeetWeb/menusapp/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+class Cliente(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.usuario.username
+
+class Administrador(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.usuario.username
+
+class Menu(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=0)
+
+    def __str__(self):
+        return self.nombre
+    
+class Boleta(models.Model):
+    ESTADOS = (
+        ('pendiente', 'Pendiente'),
+        ('aprobado', 'Aprobado'),
+        ('rechazado', 'Rechazado'),
+    )
+    usuario = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='boletas')
+    menu = models.ForeignKey(Menu, on_delete=models.SET_NULL, null=True, blank=True, related_name='boletas')
+    estado = models.CharField(max_length=10, choices=ESTADOS, default='pendiente')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Boleta {self.id} - Usuario: {self.usuario.username} - Estado: {self.estado}"
+    
+
+
+
+class Carrito(models.Model):
+    usuario = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Carrito de {self.usuario.username}"
+
+class ItemCarrito(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE, related_name='items')
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.cantidad} de {self.menu.nombre} en el carrito de {self.carrito.usuario.username}"
+
+    def get_cost(self):
+        return self.precio * self.cantidad
